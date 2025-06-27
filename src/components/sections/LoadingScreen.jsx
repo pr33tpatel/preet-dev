6a19b394ca93ec3921dbafb241d3a8b76e643a5a
Preet Patel <preetpatel1223@gmail.com>
2025-06-27 18:27:53 -0400

    changed loading screen text to 'turn your dreams into reality'

--- src/components/sections/LoadingScreen.jsx
+++ src/components/sections/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "< Hello World >";
+  const fullText = "< turn your dreams in reality >";
 
   useEffect(() => {
     let index = 0;
